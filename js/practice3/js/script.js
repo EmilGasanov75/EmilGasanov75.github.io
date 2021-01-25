486e270322bb025c339c92887012aa98a68046a5
EmilGasanov75 <emil.hasanov.gamer@gmail.com>
2021-01-25 18:06:10 +0200

    js

---
+++ js/practice3/js/script.js
@@ -0,0 +1,53 @@
+//const k1 = Number.parseInt('5px');
+
+//const k2 = Number.parseInt('12djd334');
+
+//const k3 = Number.parseInt('12.45asdwe12');
+
+//const k4 = Number.parseInt('qwqweeewq');
+
+//console.log(k1);
+
+//console.log(k2);
+
+//console.log(k3);
+
+//console.log(k4);
+
+// const k1 = Number.parseFloat('5px');
+
+// const k2 = Number.parseFloat('12djd334');
+
+// const k3 = Number.parseFloat('12.45asdwe12');
+
+// const k4 = Number.parseFloat('qwqweeewq');
+
+// console.log(k1);
+
+// console.log(k2);
+
+// console.log(k3);
+
+// console.log(k4);
+
+// console.log(Math.min(2, 34, 99, 3, 22, 36, 733, 18));
+
+// console.log(Math.max(2, 34, 99, 3, 22, 36, 733, 18));
+
+// console.log((Math.random) * (19 - 3) + 1);
+
+// let result = 5+5+'5';
+
+// console.log(typeof(result));
+
+// let email = 'emil.hasanov.gamer@gmail.com';
+
+// console.log(email.length)
+
+// alert(email.includes("@"))
+
+// const name = 'Stas';
+
+// alert(`Дякую. ${name}, до сплати 300 грн`);    
+
+prompt('Ваше імя?')
---
+++ js/practice4/js/script.js
@@ -0,0 +1,33 @@
+// let TodayNumberOfMonth = 23;
+
+// if (TodayNumberOfMonth === 1) {
+//     console.log('Sogodni 1 chislo')
+// }
+
+// let TodayNumberOfMonth = 23;
+
+// if (TodayNumberOfMonth % 2 === 0) {
+//     console.log('sogodni parne chislo');
+// }
+// else {
+//     console.log('sogodni neparne chislo');
+// }
+
+//  if (5 % 2 === 0) {
+//      console.log(5 % 2 + 'грн');
+//  }
+//  else {
+//      console.log(5 % 2 + 'євро');
+//  }
+
+const age=55;
+
+let StAgeGroup= (age => 0 && < 17);
+
+let ndAgeGroup= (age =>17 && < 61);
+
+let rd
+AgeGroup= (age =>61 && < 101);
+
+
+
